a482d6397826b612662bcc55a765d6167e528fa5
chrshih875 <chrshih875@gmail.com>
2024-06-02 00:58:48 -0700

    respond with 200

--- app/main.py
+++ app/main.py
@@ -5,7 +5,14 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    while True:
+        conn, address = server_socket.accept() # wait for client
+        print("conn", conn)
+        print("addy", address)
+        request = conn.recv(1024)
+        if request:
+            conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+    
 
 
 if __name__ == "__main__":
